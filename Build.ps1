8ea727222fda19ffe21cd0fb3bbad37db12270b4
Antonín Procházka <antonin@postsharp.net>
2024-08-05 16:37:16 +0200

    Build fixed, test project added.

--- Build.ps1
+++ Build.ps1
@@ -1,11 +1,10 @@
-(& dotnet nuget locals http-cache -c) | Out-Null
-
 Push-Location -Path "$PSScriptRoot\eng\src"
 $exitCode = -1;
 
 try {
-    # We run the command in the engineering directory, so the global.json from that directory is used.
+    # We run the commands in the engineering directory, so the global.json from that directory is used.
     # Builds are then execured in the project/solution directory using the --project-dir-as-working-dir, so the possibly modified global.json in the repository root is used.
+    (& dotnet nuget locals http-cache -c) | Out-Null
     & dotnet run --project "BuildMetalamaTestsDotNetSdk.csproj" -- $args --project-dir-as-working-dir
     $exitCode = $LASTEXITCODE
 } finally {
---
+++ src/Metalama.Tests.DotNetSdk/AlterResultAttribute.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Framework.Aspects;
+
+namespace Metalama.Tests.DotNetSdk;
+
+internal class AlterResultAttribute : OverrideMethodAspect
+{
+    public override dynamic? OverrideMethod() => meta.Proceed() + 1;
+}
