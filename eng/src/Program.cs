0929e59de04bbe881e818413bb6bb7a3043a3514
Antonín Procházka <prochan2@users.noreply.github.com>
2024-08-05 12:44:26 +0200

    Initial commit

---
+++ eng/src/Program.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
+using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
+using Spectre.Console.Cli;
+using Dependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.TemplateDependencies;
+using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2024_2;
+
+var product = new Product( Dependencies.MyProduct )
+{
+    Solutions = [new DotNetSolution( "My.Product.sln" )],
+    PublicArtifacts = Pattern.Create( "My.Product.$(PackageVersion).nupkg" ),
+    Dependencies = [DevelopmentDependencies.PostSharpEngineering, MetalamaDependencies.Metalama]
+};
+
+var commandApp = new CommandApp();
+
+commandApp.AddProductCommands( product );
+
+return commandApp.Run( args );
