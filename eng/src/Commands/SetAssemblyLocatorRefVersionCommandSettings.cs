d893bb27e5fc9c2ee37eedd31df1dcac94af5a8b
Antonín Procházka <antonin@postsharp.net>
2024-08-26 15:49:40 +0200

    Investigating Linux failing build.

--- eng/src/Commands/SetAssemblyLocatorRefVersionCommandSettings.cs
+++ eng/src/Commands/SetAssemblyLocatorRefVersionCommandSettings.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+using JetBrains.Annotations;
 using PostSharp.Engineering.BuildTools;
 using Spectre.Console.Cli;
 using System.ComponentModel;
 
 namespace BuildMetalamaTestsDotNetSdk.Commands;
 
-public class SetAssemblyLocatorRefVersionCommandSettings : CommonCommandSettings
+[UsedImplicitly]
+internal class SetAssemblyLocatorRefVersionCommandSettings : CommonCommandSettings
 {
     [Description( "The version of .NET SDK. E.g. 6.0.302." )]
     [CommandArgument( 0, "<sdk-version>" )]
