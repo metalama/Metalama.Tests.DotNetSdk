5067a93c7810222d9a49382e1e484369150abc9a
Antonín Procházka <antonin@postsharp.net>
2024-08-27 14:25:59 +0200

    Ansi console disabled.

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,5 @@
-$logtimestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$logfilename = "$PSScriptRoot/Transcript_$logtimestamp.log"
+# $logtimestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+# $logfilename = "$PSScriptRoot/Transcript_$logtimestamp.log"
 
 Push-Location -Path "$PSScriptRoot/eng/src"
 $exitCode = -1;
@@ -8,11 +8,11 @@ try {
     # We run the commands in the engineering directory, so the global.json from that directory is used.
     # Builds are then execured in the project/solution directory using the --project-dir-as-working-dir, so the possibly modified global.json in the repository root is used.
     (& dotnet nuget locals http-cache -c) | Out-Null
-    & dotnet run --project "BuildMetalamaTestsDotNetSdk.csproj" -- $args --project-dir-as-working-dir > $logfilename 2>&1
+    & dotnet run --project "BuildMetalamaTestsDotNetSdk.csproj" -- $args --project-dir-as-working-dir # > $logfilename 2>&1
     $exitCode = $LASTEXITCODE
 } finally {
     Pop-Location
-    [Console]::WriteLine([System.IO.File]::ReadAllText($logfilename))
+    # [Console]::WriteLine([System.IO.File]::ReadAllText($logfilename))
 }
 
 exit $exitCode
