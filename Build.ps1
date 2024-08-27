c814c2efbd62362aceb498dc852acd77bcfd23e1
Antonín Procházka <antonin@postsharp.net>
2024-08-27 11:10:28 +0200

    Console enhancements disabled.

--- Build.ps1
+++ Build.ps1
@@ -5,7 +5,7 @@ try {
     # We run the commands in the engineering directory, so the global.json from that directory is used.
     # Builds are then execured in the project/solution directory using the --project-dir-as-working-dir, so the possibly modified global.json in the repository root is used.
     (& dotnet nuget locals http-cache -c) | Out-Null
-    & dotnet run --project "BuildMetalamaTestsDotNetSdk.csproj" -- $args --project-dir-as-working-dir 2>&1 | Tee-Object -FilePath $PSScriptRoot/build_output.log
+    & dotnet run --project "BuildMetalamaTestsDotNetSdk.csproj" -- $args --project-dir-as-working-dir
     $exitCode = $LASTEXITCODE
 } finally {
     Pop-Location
