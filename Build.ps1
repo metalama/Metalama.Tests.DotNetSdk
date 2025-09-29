9a6a10a70c208c14082093d630499180ac88ab1a
Gael Fraiteur <gael@postsharp.net>
2025-09-29 12:01:20 +0200

    Updated to new engineering and 2025.1.

--- Build.ps1
+++ Build.ps1
@@ -1,14 +1,66 @@
-Push-Location -Path "$PSScriptRoot/eng/src"
-$exitCode = -1;
+# The original of this file is in the PostSharp.Engineering repo.
+# You can generate this file using `./Build.ps1 generate-scripts`.
 
-try {
-    # We run the commands in the engineering directory, so the global.json from that directory is used.
-    # Builds are then execured in the project/solution directory using the --project-dir-as-working-dir, so the possibly modified global.json in the repository root is used.
-    (& dotnet nuget locals http-cache -c) | Out-Null
-    & dotnet run --project "BuildMetalamaTestsDotNetSdk.csproj" -- $args --project-dir-as-working-dir
-    $exitCode = $LASTEXITCODE
-} finally {
-    Pop-Location
+[CmdletBinding(PositionalBinding = $false)]
+param(
+    [switch]$Interactive, # Opens an interactive PowerShell session
+    [switch]$StartVsmon, # Enable the remote debugger.
+    [Parameter(ValueFromRemainingArguments)]
+    [string[]]$BuildArgs   # Arguments passed to `Build.ps1` within the container.
+)
+
+####
+# These settings are replaced by the generate-scripts command.
+$EngPath = 'eng'
+$ProductName = 'MetalamaTestsDotNetSdk'
+####
+
+if ($StartVsmon)
+{
+    $vsmonport = 4024
+    Write-Host "Starting Visual Studio Remote Debugger, listening at port $vsmonport." -ForegroundColor Cyan
+    $vsmonProcess = Start-Process -FilePath "C:\msvsmon\msvsmon.exe" `
+        -ArgumentList "/noauth","/anyuser","/silent","/port:$vsmonport","/timeout:2147483647" `
+        -NoNewWindow -PassThru
 }
 
-exit $exitCode
+# Change the prompt and window title in Docker.
+if ($env:RUNNING_IN_DOCKER)
+{
+    function global:prompt
+    {
+        $host.UI.RawUI.WindowTitle = "[docker] " + (Get-Location).Path
+        "[docker] $( Get-Location )> "
+    }
+}
+
+
+if (-not $Interactive -or $BuildArgs)
+{
+    # Change the working directory so we can use a global.json that is specific to eng.
+    $previousLocation = Get-Location
+
+    Set-Location $PSScriptRoot\$EngPath\src
+
+    try
+    {
+
+        # Run the project.
+        & dotnet run --project "$PSScriptRoot\$EngPath\src\Build$ProductName.csproj" -- $BuildArgs
+
+        if ($StartVsmon)
+        {
+            Write-Host ""
+            Write-Host "Killing vsmon.exe."
+            $vsmonProcess.Kill()
+        }
+    }
+    finally
+    {
+        Set-Location $previousLocation
+    }
+}
+
+if ( $Interactive ) {
+    Write-Host "Entering interactive PowerShell." -ForegroundColor Green
+}
--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -5,19 +5,19 @@ using BuildMetalamaTestsDotNetSdk.Helpers;
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
-using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
-using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2025_0;
+using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2025_1;
 
 var product = new Product( MetalamaDependencies.DotNetSdkTests )
 {
+    GenerateNuGetConfig = true,
+    GenerateTeamCitySettings = false,
+    DotNetSdkVersion = new DotNetSdkVersion("9.0.300") { AllowPrerelease = true },
     Solutions = [new DotNetSolution( ProjectInfo.ProjectPath ) { BuildMethod = BuildMethod.Build }],
-    Dependencies = [DevelopmentDependencies.PostSharpEngineering, MetalamaDependencies.Metalama]
 };
 
 var app = new EngineeringApp( product );
 
 var data = new BaseCommandData( product );
-app.Configure( c => c.AddCommand<SetDotNetSdkVersionCommand>( "set-sdk-version" ).WithData( data ) );
 app.Configure( c => c.AddCommand<CreateProjectCommand>( "create-project" ).WithData( data ) );
 app.Configure( c => c.AddCommand<VerifyTransformationsCommand>( "verify-transformations" ).WithData( data ) );
 app.Configure( c => c.AddCommand<SetAssemblyLocatorRefVersionCommand>( "set-ref-version" ).WithData( data ) );
