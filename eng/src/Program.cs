7015639c1fc1840fda4b385e5f30afd072196692
Gael Fraiteur <gael@postsharp.net>
2026-02-01 17:48:49 +0100

    Eng: using preferred .NET versions, updated .gitignore.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -3,6 +3,7 @@
 using BuildMetalamaTestsDotNetSdk.Commands;
 using BuildMetalamaTestsDotNetSdk.Helpers;
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2026_1;
@@ -11,7 +12,7 @@ var product = new Product( MetalamaDependencies.DotNetSdkTests )
 {
     GenerateNuGetConfig = true,
     GenerateTeamCitySettings = false,
-    DotNetSdkVersion = new DotNetSdkVersion("9.0.300") { AllowPrerelease = true },
+    DotNetSdkVersion = new DotNetSdkVersion( PreferredVersions.DotNetSdk.V_9_0 ) { AllowPrerelease = true },
     Solutions = [new DotNetSolution( ProjectInfo.ProjectPath ) { BuildMethod = BuildMethod.Build }],
 };
 
